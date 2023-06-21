a961fb428f7777485ee197e06a8b3cb65d9ed433
Jeremy Lo <ccjeremy.lo@gmail.com>
2023-06-21 22:40:36 +0100

    added more tests, refactored pricing method, and re-wrote sde method of the BSModel

--- python/ExoticEngine/MarketDataObject/Parameter.py
+++ python/ExoticEngine/MarketDataObject/Parameter.py
@@ -21,7 +21,7 @@ class Parameter:
         return sci.integrate.quad(self._param, t1, t2)[0] / (t2 - t1)
 
     def get_root_mean_square(self, t1: float, t2: float) -> float:
-        """returns: int_t1^t2 param(t)^2 dt / (t2-t1)"""
+        """returns: sqrt[ int_t1^t2 param(t)^2 dt / (t2-t1) ]"""
         assert t2 >= t1
         assert t1 >= 0
         integrand = lambda t: self._param(t) ** 2
---
+++ python/sandbox/pricer_helper.py
@@ -0,0 +1,27 @@
+from ExoticEngine.Statistics import RandomNumberGenerators as RNG
+from ExoticEngine.MarketDataObject import Parameter as P
+from ExoticEngine.Statistics import Statistics as Stats
+
+
+
+def build_constant_market_param(rate: float,
+                                vol: float,
+                                repo: float,
+                                div: float):
+    Rate = P.Parameter(param=lambda t: rate)
+    Vol = P.Parameter(param=lambda t: vol)
+    Repo = P.Parameter(param=lambda t: repo)
+    Div = P.Parameter(param=lambda t: div)
+    return {"Rate": Rate, "Vol": Vol, "Repo": Repo, "Div": Div}
+
+
+def build_RNG(random_number_type: str):
+    random_number = RNG.RandomNumberType(random_number_type)
+    return RNG.TestRandom(random_number_type=random_number)
+
+
+
+def build_collector(condition_type: str, criteria: float):
+    condition = Stats.ConditionType(condition_type)
+    termination_condition = Stats.TerminationCondition(condition, criteria)
+    return Stats.GetStatistics(termination_condition)
